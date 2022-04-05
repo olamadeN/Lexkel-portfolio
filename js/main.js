f80d136c9b390fa473f960e4b69e6784fbfd0a6f
nissi <olamademuyiwa@gmail.com>
2022-04-05 13:28:02 +0100

    isaacc's correction implemented
    Signed-off-by: nissi <olamademuyiwa@gmail.com>

--- js/main.js
+++ js/main.js
@@ -3,39 +3,51 @@
 
 function show_list() {
     var show = document.getElementById("drop-list");
-
+    var arrow = document.getElementById("caret-down-1");
+    
+    arrow.style.transform = "rotate(180deg)";
+    
     if (show.style.display == "block") {
         show.style.display = "none";
     } else {
         show.style.display = "block";
     }
+    
 }
 function show_list_2() {
     var show_2 = document.getElementById("drop-list-2");
+    var arrow = document.getElementById("caret-down-2");
+    
+    arrow.style.transform = "rotate(180deg)";
 
     if (show_2.style.display == "block") {
         show_2.style.display = "none";
+        
     } else {
         show_2.style.display = "block";
     }
 }
 function show_list_3() {
     var show_3 = document.getElementById("drop-list-3");
-
+    var arrow = document.getElementById("caret-up-1");
     if (show_3.style.display == "none") {
         show_3.style.display = "block";
     } else {
         show_3.style.display = "none";
     }
+    arrow.style.transform = "rotate(180deg)";
 }
 function show_list_4() {
     var show_4 = document.getElementById("drop-list-4");
-
+    var arrow = document.getElementById("caret-up-2");
     if (show_4.style.display == "none") {
         show_4.style.display = "block";
     } else {
         show_4.style.display = "none";
     }
+
+    arrow.style.transform = "rotate(180deg)";
+
 }
 /*
 window.onclick = function (event) {
